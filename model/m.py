d4eccfa8c559161e93d8ce94b9ef333851271965
makinyilmaz <77252309+makinyilmaz@users.noreply.github.com>
2021-01-11 03:40:42 +0300

    dfpn model and validation scripts added

---
+++ model/m.py
@@ -0,0 +1,113 @@
+
+# coding: utf-8
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.nn import init
+import math
+import torchvision.ops.deform_conv as df
+from .cbam import CBAM
+
+
+
+def conv_same(in_ch,out_ch,kernel_size):
+    conv = nn.Conv2d(in_channels=in_ch,out_channels=out_ch,kernel_size=kernel_size,padding=kernel_size//2,bias=True)
+    return conv
+
+
+class RDB(nn.Module):
+    def __init__(self, channel):
+        super(RDB, self).__init__()
+        
+        self.cnn1 = conv_same(channel,channel,3)
+        self.cnn2 = conv_same(channel*2,channel,3)
+        self.cnn3 = conv_same(channel*3,channel,3)
+        self.down_conv = conv_same(channel*4,channel,1)
+        
+    def forward(self,x):
+        c1 = F.relu(self.cnn1(x))
+        c2 = F.relu(self.cnn2(torch.cat((x,c1),dim=1)))
+        c3 = F.relu(self.cnn3(torch.cat((x,c1,c2),dim=1)))
+        concat = torch.cat((x,c1,c2,c3),dim=1)
+        out = self.down_conv(concat) + x
+
+        return out
+
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        
+        
+        self.feature_ext = nn.Sequential(
+            conv_same(1,64,3),
+            RDB(64),
+            RDB(64),
+            conv_same(64, 64, 3)
+        )
+        
+        self.bottleneck = nn.Sequential(
+            conv_same(64*4, 64, 3),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            conv_same(64, 64, 3)
+        )
+        
+        
+        self.conv_offset = conv_same(64, 2*3*3*4, 3)
+        self.conv_offset.weight.data.zero_()
+        self.conv_offset.bias.data.zero_() 
+        
+        self.dcn1 = df.DeformConv2d(64, 64, kernel_size=3, padding=1)
+        self.dcn2 = df.DeformConv2d(64, 64, kernel_size=3, padding=1)
+        self.dcn3 = df.DeformConv2d(64, 64, kernel_size=3, padding=1)
+        self.dcn4 = df.DeformConv2d(64, 64, kernel_size=3, padding=1)
+    
+        self.rec = nn.Sequential(
+            conv_same(64*4, 64, 3),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            RDB(64),
+            conv_same(64, 1, 3)
+        )
+        
+    def forward(self,x):
+        x1 = x[:,0:1]
+        x2 = x[:,1:2]
+        x3 = x[:,2:3]
+        x4 = x[:,3:4]
+        
+        f1 = self.feature_ext(x1)
+        f2 = self.feature_ext(x2)
+        f3 = self.feature_ext(x3)
+        f4 = self.feature_ext(x4)
+
+        b = self.bottleneck(torch.cat([f1,f2,f3,f4], dim=1))
+        offset1, offset2, offset3, offset4 = torch.chunk(self.conv_offset(b), 4, dim=1)
+        d1 = self.dcn1(f1, offset1)     
+        d2 = self.dcn2(f2, offset2)     
+        d3 = self.dcn3(f3, offset3)     
+        d4 = self.dcn4(f4, offset4)  
+        att = torch.cat([d1,d2,d3,d4], dim=1)
+        y_pred = self.rec(att)
+        
+        return y_pred, (torch.mean(offset1**2) + torch.mean(offset2**2) + torch.mean(offset3**2) + torch.mean(offset4**2))/4.
+        
---
+++ validate.py
@@ -0,0 +1,140 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+import numpy as np
+import glob
+import os
+import itertools
+import warnings
+warnings.filterwarnings('ignore')
+import imageio
+from natsort import natsorted
+import logging
+import time
+from model import m
+
+
+
+logging.basicConfig(filename="result.log",level=logging.INFO)
+
+device = torch.device("cuda:1")
+
+def test_data_loader(path="mpeg/"):
+    folders = natsorted(glob.glob(path+"*"))
+    for f in folders:
+        im_list = natsorted(glob.glob(f+"/*"))
+        yield(im_list)
+
+
+def float_to_uint8(image):
+    clip = np.clip(image,-1,1)
+    original_range = np.round((clip*127.5)+127.5)
+    im_uint8 = np.uint8(original_range)
+    return im_uint8
+
+
+def prepare_test_data(test_im_list):
+    X_test = []
+    Y_test = []
+    
+    size = 4
+    
+    for k in range(len(test_im_list)-size):
+        x_test_array = []
+        for i in range(size):
+            
+            x_image = imageio.imread(test_im_list[k+i])
+            (nw,nh) = x_image.shape
+            x_test_array.append(x_image)
+        
+        X_test.append(x_test_array)
+        y_image = imageio.imread(test_im_list[k+i+1])
+        Y_test.append(y_image.reshape(1,nw,nh))
+    
+    return np.array(X_test),np.array(Y_test)
+
+def normalize(tensor):
+    norm = (tensor-127.5)/127.5
+    return norm
+
+
+def test_and_save(model):
+    with torch.no_grad():
+        
+        model.eval()
+
+
+        test_im_batch = test_data_loader()
+
+        num_of_test_videos = 0
+        folder_names = ["coastguard","container","football","foreman","garden","hall_monitor","mobile","tennis"]
+        
+        folder = 0
+        total_frames = 0
+
+        time_start = time.time()
+        for test_ims in test_im_batch:
+            X_test,Y_test = prepare_test_data(test_ims)
+            
+            (m,nc,nw,nh) = Y_test.shape
+
+
+            d = 5
+            os.makedirs("Results/"+folder_names[folder],exist_ok=True)
+
+            for frame in range(m):
+                inp = normalize(torch.from_numpy(X_test[frame:frame+1]).to(device).float())
+                real = normalize(torch.from_numpy(Y_test[frame:frame+1]).to(device).float())
+
+
+                out, norm = model.forward(inp)
+
+                uint8_real = float_to_uint8(real.cpu().numpy()[0,0])
+                uint8_out = float_to_uint8(out.cpu().detach().numpy()[0,0])
+
+                save_path = "Results/"+folder_names[folder]+"/frame"+str(d)+".png"
+                imageio.imsave(save_path,uint8_out)
+                d += 1
+                total_frames += 1
+                
+            folder += 1
+            
+        time_end = time.time()
+
+        test_time = time_end - time_start
+
+        fps = total_frames / test_time
+    
+    return fps
+
+
+def load_pre_model(m, path, requires_grad):
+    pre_dict = torch.load(path, map_location=lambda storage, loc: storage)["state_dict"]
+    m_dict = m.state_dict()
+    m_dict.update(pre_dict)
+    m.load_state_dict(m_dict)
+    for p in m.parameters():
+        p.requires_grad = requires_grad
+    return m
+
+def main():
+    path = "dfpn.pth"
+    
+    model = m.Model()
+    model.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage)["state_dict"])
+    for p in model.parameters():
+        p.requires_grad = False
+    model = model.to(device).float()
+        
+    test_fps = test_and_save(model)
+            
+    logging.info("fps: "+str(test_fps))
+
+        
+
+main()
+
